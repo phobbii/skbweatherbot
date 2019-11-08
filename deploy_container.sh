45099ff718274086e87d373e4ee3f9fc4c36aa50
Yevhen Skyba <Yevhen.Skyba@teaminternational.com>
2019-11-08 21:39:02 +0200

    Added deployment script

---
+++ deploy_container.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+set -E
+
+# Set colours
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+PURPLE=`tput setaf 6`
+RESET=`tput sgr0`
+
+# Ensure we can exit if a subshell returns error
+stderr() {
+    echo "${GREEN}${1}${RESET}" >&2
+}
+
+# Print error and exit with fault code 2
+error() {
+    stderr "${RED}ERROR: ${1}${RESET}"
+    exit 2
+}
+
+# Print if debug is enabled
+debug() {
+        [[ "${DEBUG}" == ${TRUE} ]] && stderr "DEBUG: ${1}"
+}
+
+# Extract the value from an argument, and exit with error if no value is present
+argval() {
+        debug "${1} ${2}"
+        [[ -z "${2}" || "${2}" =~ ^-- ]] && error "Missing value for argument: ${1}"
+        echo "${2}"
+}
+
+# Help/usage
+if [[ "${1}" == '-h' || "${1}" == '--help' ]]; then
+    echo ${PURPLE}
+    cat <<EOH
+Usage: $(basename $0) --owm KEY --telegram KEY --port PORT
+
+Pay attention instance must have public ip and open https port!
+
+Required options:
+  --owm STR          OWM API key
+  --telegram STR     Telegram API key
+  --port INT         HTTPS port
+EOH
+    exit 0
+    echo ${RESET}
+fi
+
+# Parse all arguments passed to the script
+while [[ "${#}" -gt 0 ]]; do
+        ARG="${1}"
+        shift
+        case "${ARG}" in
+                --owm)
+                        OWM_KEY=$(argval "${ARG}" "${1}"); shift
+                        ;;
+                --telegram)
+                        TELEBOT_KEY=$(argval "${ARG}" "${1}"); shift
+                        ;;
+                --port)
+                        HTTPS_PORT=$(argval "${ARG}" "${1}"); shift
+                        ;;
+        esac
+done
+
+# Check if we got the required values
+[[ -z "${OWM_KEY}" ]] && error "Missing argument: --owm"
+[[ -z "${TELEBOT_KEY}" ]] && error "Missing argument: --telegram"
+[[ -z "${HTTPS_PORT}" ]] && error "Missing argument: --port"
+[[ ! "${HTTPS_PORT}" =~ ^[0-9]+$ || "${#HTTPS_PORT}" -lt 2 || "${#HTTPS_PORT}" -gt 5 ]] && error "Invalid port: ${HTTPS_PORT}"
+
+# Main block
+if [[ $(uname -s) -eq "Linux" ]]; then
+    if [[ $EUID -ne 0 ]]; then
+        echo "${RED}Please run as root.${RESET}"
+        exit
+    else
+        if [[ $(compgen -c | grep -x "docker") ]]; then
+            if [[ -f $(find . -maxdepth 1 -name "Dockerfile") ]] && [[ -f $(find . -maxdepth 1 -name "*.py") ]]; then
+                sed -i -e "5,8 d" -e "4 a\ENV OWN_KEY $OWM_KEY\nENV TELEBOT_KEY $TELEBOT_KEY\nENV WEBHOOK_HOST $(curl -s ifconfig.co)\nENV WEBHOOK_PORT $HTTPS_PORT" Dockerfile
+                echo "${GREEN}Dockerfile successfully updated.${RESET}"
+                echo "${GREEN}Building docker image.${RESET}"
+                docker build -t skbweatherbot . > /dev/null 2>&1
+                if [[ -n $(docker images | grep "skbweatherbot" | awk '{print $3}') ]]; then
+                    echo "${GREEN}Bot image built successfully${RESET}"
+                    docker run -d --restart=always --name skbweatherbot -p $HTTPS_PORT:$HTTPS_PORT $(docker images | grep "skbweatherbot" | awk '{print $3}') > /dev/null 2>&1
+                    if [[ $(docker ps -a | grep skbweatherbot | awk '{print $8}') == "Up" ]]; then
+                        echo "${GREEN}Bot container deployed successfully${RESET}"
+                    else
+                        echo "${RED}Bot container deployed unsuccessfully.${RESET}"
+                        exit
+                    fi
+                else
+                    echo "${RED}Bot image built unsuccessfully.${RESET}"
+                    exit
+                fi
+            else
+                echo "${RED}Either Dockerfile or skbweatherbot is not exist in current directory.${RESET}"
+                exit
+            fi
+        else
+            echo "${RED}Docker not installed. Please install docker and re-run script.${RESET}"
+            exit
+        fi
+    fi
+else
+    echo "${RED}Your platform ($(uname -a)) is not supported.${RESET}"
+    exit 1
+fi
