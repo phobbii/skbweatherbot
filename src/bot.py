0c910180e8f8f405b36a6e76f26fef9eb0319aa9
Yevhen Skyba <skiba.eugene@gmail.com>
2026-02-14 10:36:23 +0200

    Added newlines

--- src/bot.py
+++ src/bot.py
@@ -34,6 +34,7 @@ callback_handlers = CallbackHandlers(bot, cmd_handlers)
 app = web.Application()
 executor = ThreadPoolExecutor(max_workers=4)
 
+
 async def handle_webhook(request: web.Request) -> web.Response:
     """Handle incoming webhook requests."""
     if request.match_info.get('token') == bot.token:
@@ -50,38 +51,47 @@ async def handle_webhook(request: web.Request) -> web.Response:
 
 app.router.add_post('/{token}/', handle_webhook)
 
+
 # Register handlers
 @bot.message_handler(commands=['start'])
 def start_command(message: telebot.types.Message) -> None:
     cmd_handlers.handle_start(message)
 
+
 @bot.message_handler(commands=['location'])
 def location_command(message: telebot.types.Message) -> None:
     cmd_handlers.handle_location(message)
+
 
 @bot.message_handler(commands=['forecast'], content_types=config.CONTENT_TO_HANDLE)
 def forecast_command(message: telebot.types.Message) -> None:
     cmd_handlers.handle_forecast_command(message)
 
+
 @bot.message_handler(commands=['help'])
 def help_command(message: telebot.types.Message) -> None:
     cmd_handlers.handle_help(message)
+
 
 @bot.message_handler(commands=['author'])
 def author_command(message: telebot.types.Message) -> None:
     cmd_handlers.handle_author(message)
 
+
 @bot.callback_query_handler(func=lambda c: True)
 def callback_query(callback: telebot.types.CallbackQuery) -> None:
     callback_handlers.handle_callback(callback)
+
 
 @bot.message_handler(func=lambda m: True, content_types=config.CONTENT_TO_HANDLE)
 def weather_message(message: telebot.types.Message) -> None:
     msg_handlers.handle_weather_request(message)
 
+
 @bot.message_handler(func=lambda m: True, content_types=config.CONTENT_TO_REJECT)
 def wrong_content_message(message: telebot.types.Message) -> None:
     msg_handlers.handle_wrong_content(message)
+
 
 def main() -> None:
     """Main function to start the bot."""
